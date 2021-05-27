6ef4c759fbef0e53fc8236755784bc11157c5c25
ItaloRFeitosa <italo85199@gmail.com>
2021-05-26 21:27:39 -0300

    feat: create Dashboard Page

---
+++ apps/dashboard/src/app/dashboard/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { H1, Dark } from '@nx-netlify-deploy-poc/ui'
+
+const Container = styled.div`
+  width: 100%;
+  color: ${Dark.colors.white};
+  padding-top: 48px;
+`
+
+export function Dashboard(){
+  return <Container>
+      <H1>This is the page of Dashboard</H1>
+  </Container>
+}
