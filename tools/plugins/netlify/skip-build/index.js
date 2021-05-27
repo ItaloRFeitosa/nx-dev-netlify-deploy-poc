359f199b8a155941ae8eb57e4101c45395883f58
ItaloRFeitosa <italo85199@gmail.com>
2021-05-27 11:41:17 -0300

    feat: add contants log

--- tools/plugins/netlify/skip-build/index.js
+++ tools/plugins/netlify/skip-build/index.js
@@ -1,7 +1,8 @@
 module.exports = {
-  onPreBuild: ({ utils }) => {
+  onPreBuild: ({ utils, constants }) => {
     const currentProject = process.env.PROJECT_NAME;
     console.log("process.env", process.env)
+    console.log("constants", constants)
     const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
     const latestCommit = 'HEAD';
     const projectHasChanged = projectChanged(
