47e6d212f8a7a62657eec7651726ae71bec4c6d4
Italo Feitosa <italo85199@gmail.com>
2021-06-01 17:13:26 -0300

    feat: change main page route

--- apps/accounting/src/app/index.tsx
+++ apps/accounting/src/app/index.tsx
@@ -16,7 +16,7 @@ const makeAccounting = (user: User): SharedLayoutProps => ({
 
 const makePages = (user: User) => [{
   exact: true,
-  path: "/",
+  path: "/accounting",
   component: () => <SharedLayout {...makeAccounting(user)}/>
 }]
 
